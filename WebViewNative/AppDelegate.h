1ad110c8aea4327e78258ea986632c0f4061447a
JunPingWon <1436538028@qq.com>
2016-05-06 11:22:47 +0800

    WebViewNative
    点击h5图片，native预览图片

---
+++ WebViewNative/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebViewNative
+//
+//  Created by JunpingWon on 16/5/5.
+//  Copyright © 2016年 JunpingWon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebViewNative/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebViewNative
+//
+//  Created by JunpingWon on 16/5/5.
+//  Copyright © 2016年 JunpingWon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
